66e687d2d7228b13ead9c757db6d782478b1e00a
lanboys1225 <lanboys1225@gmail.com>
2018-07-07 11:28:45 +0800

    构建项目层级关系

---
+++ CommLib/src/main/java/com/bing/lan/lib/HelloCommLib.java
@@ -0,0 +1,12 @@
+package com.bing.lan.lib;
+
+/**
+ * Created by 蓝兵 on 2018/7/7.
+ */
+
+public class HelloCommLib {
+
+    public static void main(String[] args) {
+        System.out.print("hello lib");
+    }
+}
---
+++ MainLib/src/main/java/com/bing/lan/mainLib/HelloMainLib.java
@@ -0,0 +1,12 @@
+package com.bing.lan.mainLib;
+
+/**
+ * Created by 蓝兵 on 2018/7/7.
+ */
+
+public class HelloMainLib {
+
+    public static void main(String[] args) {
+        System.out.print("hello lib");
+    }
+}
